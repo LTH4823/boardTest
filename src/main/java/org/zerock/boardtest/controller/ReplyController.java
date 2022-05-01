418bed37347c93905f37b53e2c8891f77c23c241
LTH4823 <peter4823zx@naver.com>
2022-05-01 17:07:10 +0900

    ReplyController and ReplyDTO setting and jacksondatatype setting

--- src/main/java/org/zerock/boardtest/controller/ReplyController.java
+++ src/main/java/org/zerock/boardtest/controller/ReplyController.java
@@ -1,18 +1,33 @@
 package org.zerock.boardtest.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.boardtest.dto.ReplyDTO;
+import org.zerock.boardtest.service.ReplyService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/replies/")
 @Log4j2
+@RequiredArgsConstructor
 public class ReplyController {
+
+    private final ReplyService replyService;
 
     @GetMapping("/test")
     public String[] get1(){
         return new String[]{"aaa", "bbb", "ccc"};
+    }
+    @GetMapping(value = "/list/{bno}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ReplyDTO>  getListOfBoard(@PathVariable("bno") Integer bno){
+
+        return replyService.getListOfBoard(bno);
     }
 
 }
--- src/main/java/org/zerock/boardtest/dto/ReplyDTO.java
+++ src/main/java/org/zerock/boardtest/dto/ReplyDTO.java
@@ -1,6 +1,7 @@
 package org.zerock.boardtest.dto;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -10,6 +11,11 @@ public class ReplyDTO {
     private Integer bno;
     private String replyText;
     private String rpelyer;
+    
+    //Date 타입 은 기본 타입 데이터가 아니기 때문에 일반적으로 전달이 어려움 
+    //2,3가지 정도 방법이 있는데 그중 @를 사용하는 방법
+    @DateTimeFormat(pattern = "yyyy-mm-dd:ss")
     private LocalDate regDate;
+    @DateTimeFormat(pattern = "yyyy-mm-dd:ss")
     private LocalDate updateDate;
 }
