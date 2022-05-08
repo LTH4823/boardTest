c20a0f72539612f38f942fd9a21ec89b414a74f2
LTH4823 <peter4823zx@naver.com>
2022-05-08 23:21:47 +0900

    Transaction DB test

---
+++ src/main/java/org/zerock/boardtest/service/TimeService.java
@@ -0,0 +1,5 @@
+package org.zerock.boardtest.service;
+
+public interface TimeService {
+    void insertAll(String text);
+}
---
+++ src/main/java/org/zerock/boardtest/service/TimeServiceImpl.java
@@ -0,0 +1,23 @@
+package org.zerock.boardtest.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.boardtest.mapper.TimeMapper;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class TimeServiceImpl implements TimeService{
+
+    private final TimeMapper timeMapper;
+
+    //아래의 메소드 트랜잭션 적용 선언
+    @Transactional
+    @Override
+    public void insertAll(String text) {
+        timeMapper.insertA(text);
+        timeMapper.insertB(text);
+    }
+}
