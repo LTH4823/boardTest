a73a4a0ed801063025a1139992e0cc3eaa6748a8
LTH4823 <peter4823zx@naver.com>
2022-05-09 01:05:54 +0900

    multiple file upload setting and first jsp test finish

--- src/main/java/org/zerock/boardtest/controller/UploadController.java
+++ src/main/java/org/zerock/boardtest/controller/UploadController.java
@@ -2,17 +2,43 @@ package org.zerock.boardtest.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 @Log4j2
 public class UploadController {
 
     @PostMapping("/upload1")
-    public void upload1(MultipartFile[] files){
+    public void upload1(MultipartFile[] files) {
         log.info("======================");
         log.info(files);
+        //업로드된 파일이 있다고 가정
+        for (MultipartFile file : files) {
+            log.info(file.getOriginalFilename());
+            log.info(file.getResource());
+
+
+            try (
+                    //업로드할 파일 정보를 받기 위한 inputStream
+                    InputStream in = file.getInputStream();
+                    //업로드된 파일 정보 읽기 위한
+                    FileOutputStream fos = new FileOutputStream("C:\\upload\\" + file.getOriginalFilename());) {
+
+                FileCopyUtils.copy(in,fos);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            log.info("------------------------------");
+        }
     }
 
 }
